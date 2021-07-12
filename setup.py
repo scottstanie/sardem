d0555fa91ce20e1b1c7689d1ed51fdecf7caaae5
scott <scott.stanie@gmail.com>
2021-07-12 17:29:05 -0500

    new minor version instead

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sardem",
-    version="0.5.1",
+    version="0.6.0",
     author="Scott Staniewicz",
     author_email="scott.stanie@utexas.com",
     description="Create upsampled DEMs for InSAR processing",
